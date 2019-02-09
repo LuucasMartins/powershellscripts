f2ebf8bee1b661cac8ebe12e0a8aeef573d374a3
Lucas Martins <martins@itstransdata.com>
2019-02-09 17:00:32 -0200

    adicionando o script de git #pullrequest

---
+++ git-commit.ps1
@@ -0,0 +1,33 @@
+﻿# Variavéis
+$resp = "";
+$mensagemCommit = $args[0];
+
+if($args.Count -eq 1)
+{
+    git status;
+    git add -A;
+    git status;
+
+    $resp = read-host "Deseja efetuar o pullrequest? (S/N)";
+
+    if($resp -eq "s" -or $resp -eq "S")
+    {
+      $mensagemCommit = $mensagemCommit + " #pullrequest";  
+    }
+
+    git commit -m $mensagemCommit;
+
+    $resp = read-host "Deseja efetuar o pull? (S/N)";
+
+    if($resp -eq "s" -or $resp -eq "S")
+    {
+      git pull;
+    }
+}elseif($args.Count -gt 1)
+{
+    echo "É necessário informar a mensagem entre aspas"
+}else
+{
+    echo "É necessário informar uma mensagem para efetuar o commit";
+}
+ 
---
+++ git-utils.ps1
@@ -0,0 +1,2 @@
+﻿git status
+git log -1
